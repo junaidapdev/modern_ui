7622c937079290a93459dc8f55b72b7cc523502b
junaidapdev <junaidap.dev.com>
2021-12-25 19:48:51 +0530

    first commit

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './features.css'; 
+
+const Feautures = () => {
+    return (
+        <div>
+            Feautures  
+      </div>
+    )
+}
+
+export default Feautures
